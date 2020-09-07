dc0e059892d713bab263ef928b49f64ab47d4e76
NhatMinh <nguyennhatminh1020@gmail.com>
2020-09-07 17:08:29 +0700

    define createNullList function

--- menu.cpp
+++ menu.cpp
@@ -1,9 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-//Chua xong cai chi het :v 
 struct Book;
-struct SingleLinkedList;
-typedef struct SingleLinkedList *node;
+struct Node;
+struct List;
 struct Book{
     char id[10];
     char name[20];
@@ -11,9 +10,64 @@ struct Book{
     char publishingHouse[20];
     int  publishingYear;
     bool state;
-    void createNullList();
-    node createListFromFile();
-    Book insertFist();
+};
+struct Node{
+    Book book;
+    Node *pNext;
+};
+struct List{
+    Node *pHead;
+    Node *pTail;
+};
+Book createBook()
+{
+    char s;
+    Book book;
+    printf("Type Book Id: ");
+    scanf("%s",&book.id);
+    fflush(stdin);
+    printf("Type Book Name: ");
+    scanf("%s",&book.name);
+    fflush(stdin);
+    printf("Type Book Author: ");
+    scanf("%s",&book.author);
+    fflush(stdin);
+    printf("Type Publishing House: ");
+    scanf("%s",&book.publishingHouse);
+    fflush(stdin);
+    printf("Type Publishing Year: ");
+    scanf("%d",&book.publishingYear);
+    fflush(stdin);
+    printf("Has the book been borrowed?(Y/N): ");
+    scanf("%c",&s);
+    fflush(stdin);
+    if(s == 'Y' || s == 'y') book.state = true;
+    else book.state = false;
+    return book;
+}
+Node *createNode(){
+    Node *temp = (Node*)malloc(sizeof(struct Node));
+    temp->book = createBook();
+    temp->pNext = NULL;
+    return temp;
+}
+void createNullList(List &l)
+{
+	l.pHead = NULL;
+	l.pTail = NULL;
+}
+void insertFist(List &l){
+    Node *temp = createNode();
+    if(l.pHead == NULL){
+        l.pHead = l.pTail = temp;
+    }
+    else{
+        temp->pNext = l.pHead;
+        l.pHead = temp;
+    }
+}
+    
+    
     Book insertAfter();
     Book insertLast();
     Book removeByCode();
@@ -31,17 +85,8 @@ struct Book{
     Book viewByPublishYear();
     Book sort();
     void input();
-};
-struct SingleLinkedList{
-    Book book;
-    node *pHead;
-    node *pTail;
-    node *pNext;
-};
-
 int main()
 {
-    node temp;
-    temp = (node)malloc(sizeof(struct SingleLinkedList));
+    
     return 0;
 }
